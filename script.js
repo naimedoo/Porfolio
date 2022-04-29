0eb04857a191d8c864947c46073de3fb5b80db8a
naim edoo <naim.e2703@gmail.com>
2022-04-29 14:26:36 +0200

    Portfolio

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-const darkMode = document.getElementById('dark-mode');
+// const darkMode = document.getElementById('dark-mode');
 
-darkMode.addEventListener('change', () => {
-    document.body.classList.toggle('dark');
-}) 
+// darkMode.addEventListener('change', () => {
+//     document.body.classList.toggle('dark');
+// }) 
 
+
+const card = () => {
+    let cards = document.querySelectorAll(".content")
+    for(let i = O; i < cards.length;i++) {
+        const windowHeight = window.innerHeight;
+        const elementTop = cards[i].getBoundingClientRect().top;
+        const elementVisible = 250;
+
+        if(elementTop < windowHeight - elementVisible) {
+            cards[i].classList.add("active");
+        } else {
+            cards[i].classList.remove("active")
+        }
+    }
+    
+}
+window.addEventListener("scroll", card)
